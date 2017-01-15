e09ede274daa0c4efb7536dc2fac61115bcdbfd3
matthew jenkins <drakaven@hotmail.com>
2017-01-15 22:49:33 +0000

    renamed downloa2 to download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,53 +1,25 @@
+//REQUIRED
+//process.argv
+//files extension
 
+//OPTIONS
+//fix options declarations
+//find out how to only return required values from gituhb
+//error handling
 
-var https = require('https');
 var request = require('request');
 var fs = require('fs');
-var userInfo = [];
-
+var path = 'https://api.github.com/repos/jquery/jquery/contributors?per_page=100';
+var folder = './avatars/';
 var options = {
-  host: 'api.github.com',
-  //path: `/repos/${process.argv[2]}/${process.argv[3]}/contributors`,
-  path: '/repos/drakaven/ejs/contributors',
+  url: path,
   headers: {
     'User-Agent': 'drakaven'
   }
 };
 
-const getWriteImage = function() {
-  //self executes to pass the returned function
-  //closure variable
-  var fileCounter = 0;
-  return function(user, url) {
-
-    var options = {
-      //host: url
-      host: url.match(/\/\/.*?.com/)[0].substring(2),
-      path: url.match(/com.*/)[0].substring(3)
-    }
-    var callback = function(response) {
-      var imagedata = '';
-
-      response.setEncoding('binary');
-      response.on('data', function(chunk) {
-        imagedata += chunk
-      });
-
-      response.on('end', function() {
-        fs.writeFile('./avatars/' + user, imagedata, 'binary', function(err) {
-          if (err) throw err
-          fileCounter++;
-          console.log('File saved:', user, fileCounter);
-        });
-      });
-    }
-    https.get(options, callback);
-  };
-}();
-
-
-
 const rateLimitCheck = function(response) {
+  console.log(response.headers);
   if (response.headers.status === '403 Forbidden' && response.headers['x-ratelimit-remaining'] == 0) {
     var timeDiff = new Date(response.headers['x-ratelimit-reset'] * 1000).getTime() - new Date().getTime();
     console.log("API Rate limit met, please try again in", Math.round(timeDiff / 1000 / 60) + 1, "minutes.");
@@ -55,46 +27,34 @@ const rateLimitCheck = function(response) {
   }
 }
 
-const pushUserInfo = function(resp) {
-  JSON.parse(resp).forEach((user) => {
-    userInfo.push({
-      user: user.login,
-      avatarUrl: user.avatar_url
-    });
-  });
+const getImage = function(url, login, folder) {
+  opt = options;
+  opt.url = url;
+  request(opt, function(error, response, body) {}).pipe(fs.createWriteStream(folder + login));
 }
 
-const callback = function(response) {
-  var resp = '';
-  //add a check for status 200
-  //console.log(response.headers);
-  if (rateLimitCheck(response) === false) return;
-  var i = 0;
-  var newLink = response.headers.link;
-
-  response.on('data', function(chunk) {
-    resp += chunk;
-  });
-
-  response.on('end', function() {
-    if (response.headers.link && newLink.indexOf("next") !== -1) {
-      options.path = newLink.match(/\/repositories.*?>/)[0].slice(0, -1);
-      pushUserInfo(resp);
-      console.log("Page returned");
-      resp = '';
-      https.request(options, callback).end();
-    } else {
-      pushUserInfo(resp);
+const getPages = function() {
+  var counter = 0;
+  return function(options) {
+    request(options, function(error, response, body) {
+      if (rateLimitCheck(response) === false) return;
+      var parseBody = JSON.parse(body);
       if (!fs.existsSync("./avatars")) {
         fs.mkdirSync("./avatars");
       }
-      userInfo.forEach((item) => {
-        getWriteImage(item.user, item.avatarUrl);
+      parseBody.forEach((user) => {
+        counter++;
+
+        getImage(user.avatar_url, user.login, folder);
       });
-    }
-  });
-}
 
-https.request(options, callback).end();
-
-console.log("Request sent");
+      if (response.headers.link && response.headers.link.indexOf("next") !== -1) {
+        opt = options;
+        opt.url = response.headers.link.match(/http.*?>/)[0].slice(0, -1);
+        getPages(opt);
+      }
+      console.log(counter + " Files Downloaded");
+    });
+  }
+}();
+getPages(options);
