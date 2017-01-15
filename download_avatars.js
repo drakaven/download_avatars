ec40975cd1df0a9c79d5422950d03898325e9056
matthew jenkins <drakaven@hotmail.com>
2017-01-15 07:21:42 +0000

    added directory creation

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,11 @@
 // REQUIRED
 //create url spliter of getWrite image
-//needs to make the avatars directory
 // OPTIONAL
+//make it async
 //need to make sure the extension is correct
 //try to set request size to 100
 //add 200 status check
+//return when repo not found
 
 var https = require('https');
 var fs = require('fs');
@@ -12,35 +13,35 @@ var userInfo = [];
 
 var options = {
   host: 'api.github.com',
-  path: `/repos/${process.argv[2]}/${process.argv[3]}/contributors`,
-  //path: '/repos/drakaven/ejs/contributors',
+  //path: `/repos/${process.argv[2]}/${process.argv[3]}/contributors`,
+  path: '/repos/drakaven/ejs/contributors',
   headers: {
     'User-Agent': 'drakaven'
   }
 };
 
-const getWriteImage = function (){
+const getWriteImage = function() {
   //self executes to pass the returned function
   //closure variable
-  var fileCounter =  0;
+  var fileCounter = 0;
   return function(user, url) {
+    console.log(url);
     var options = {
       host: 'www.google.com',
       path: '/images/logos/ps_logo2.png'
     }
     var callback = function(response) {
       var imagedata = ''
-      response.setEncoding('binary')
-
+      response.setEncoding('binary');
       response.on('data', function(chunk) {
         imagedata += chunk
       });
 
       response.on('end', function() {
-        fs.writeFile(user + '.png', imagedata, 'binary', function(err) {
+        fs.writeFile('./avatars/' + user + '.png', imagedata, 'binary', function(err) {
           if (err) throw err
-            fileCounter++;
-            console.log('File saved.', user + '.png', fileCounter);
+          fileCounter++;
+          console.log('File saved:', user + '.png', fileCounter);
         });
       });
     }
@@ -50,10 +51,9 @@ const getWriteImage = function (){
 
 
 
-
 const rateLimitCheck = function(response) {
   if (response.headers.status === '403 Forbidden' && response.headers['x-ratelimit-remaining'] == 0) {
-    var timeDiff = new Date(response.headers['x-ratelimit-reset'] * 1000).getTime() - new Date().getTime();
+    var timeDiff = new Date(response.headers['x-ratelimit-reset'] * 1000 + 60).getTime() - new Date().getTime();
     console.log("API Rate limit met, please try again in", Math.round(timeDiff / 1000 / 60), "minutes.");
     return false;
   }
@@ -81,13 +81,16 @@ const callback = function(response) {
 
   response.on('end', function() {
     if (response.headers.link && newLink.indexOf("next") !== -1) {
-      options.path = newLink.match(/\/repositories.*?>/)[0].slice(0, -1);;
+      options.path = newLink.match(/\/repositories.*?>/)[0].slice(0, -1);
       pushUserInfo(resp);
+      console.log("Page returned");
       resp = '';
       https.request(options, callback).end();
     } else {
       pushUserInfo(resp);
-      console.log(userInfo);
+      if (!fs.existsSync("./avatars")) {
+        fs.mkdirSync(dir);
+      }
       userInfo.forEach((item) => {
         getWriteImage(item.user, item.avatarUrl);
       });
