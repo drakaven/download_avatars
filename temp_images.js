8a5b338065d8e0c43aa81a9a0968cd2150154e7e
matthew jenkins <drakaven@hotmail.com>
2017-01-15 05:52:08 +0000

    read image from google

--- temp_images.js
+++ temp_images.js
@@ -1,18 +1,30 @@
-//image read test;
-var image = 'http://www.planwallpaper.com/static/images/9-credit-1.jpg'
+var https = require('https')
+var fs = require('fs')
+var options = {
+    host: 'www.google.com',
+    path: '/images/logos/ps_logo2.png'
+}
 
-var fs = require("fs");
 
-// Asynchronous read
-fs.readFile(process.argv[2], function(err, data) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log("Asynchronous read: " + data.toString());
-  fs.writeFile("tmp/test.txt", data, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("The file was saved!");
-  });
-});
+var callback = function(response){
+  var imagedata = ''
+    response.setEncoding('binary')
+
+    response.on('data', function(chunk){
+        imagedata += chunk
+    });
+
+}
+
+https.get(options, callback);
+
+
+
+    // res.on('end', function(){
+    //     fs.writeFile('logo.png', imagedata, 'binary', function(err){
+    //         if (err) throw err
+    //         console.log('File saved.')
+    //     })
+    // })
+
+
